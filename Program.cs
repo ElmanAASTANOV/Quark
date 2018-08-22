edb3ce479a1de37f4d3a6c160f15f47757c7ed83
Elman Astanov <elman.astanov@gmail.com>
2018-08-22 11:28:50 +0400

    Project created

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Quark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
